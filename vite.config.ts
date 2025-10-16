3c3a315baf997cb0384fed452e08931911ac5835
neominal.solutions <neominal.solutions@gmail.com>
2025-10-16 09:24:00 +0300

    terser-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		minify: 'terser', // <-- Terser kullan diyoruz (default: esbuild)
+		terserOptions: {
+			compress: {
+				drop_console: true, // console.* temizle
+				drop_debugger: true, // debugger temizle
+			},
+			format: {
+				comments: false, // yorumları da kaldır
+			},
+		},
+	},
+});
